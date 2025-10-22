d464c06d0df292a4a6a428d83fdf5d238673f9aa
Stoefoe <144897834+Stoephoe@users.noreply.github.com>
2025-10-22 20:03:02 +0200

    changed some sheit

--- 2025/OpenProp_v334/OpenProp_v3.3.4/SourceCode/Export_SolidWorks_v18.m
+++ 2025/OpenProp_v334/OpenProp_v3.3.4/SourceCode/Export_SolidWorks_v18.m
@@ -32,7 +32,7 @@ for k = 1:Mp
 
     
     for j = [1:Np,Np+2:2*Np-1,1] % (2*Np-1 points) does not double print the leading edge
-        fprintf(fid,'%f,%f,%f,\n',X3D(k,j,1)*39.3700787,Y3D(k,j,1)*-39.3700787,Z3D(k,1,1)*39.3700787);
+        fprintf(fid,'%f,%f,%f,\n',X3D(k,j,1),Y3D(k,j,1),Z3D(k,1,1));
     end
     
 %     k = 1;
